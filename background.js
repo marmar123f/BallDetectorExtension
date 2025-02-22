702af16d4dd74f40dd666e3fdbac721f55fc29e8
marmar123f <alshrqyt41@gmail.com>
2025-02-22 19:02:55 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log("Ball Detector extension running...");
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("إضافة Ball Detector تم تثبيتها بنجاح!");
+});
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["content.js"]
+    });
+});
